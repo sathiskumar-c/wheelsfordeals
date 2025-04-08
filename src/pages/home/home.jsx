83fbced223fdf1da8836c9b79d85be0aa842696b
Dev-GopiGk <gobikannan710@gmail.com>
2025-04-08 23:05:05 +0530

    08-04-25 - Our Services with Json

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,16 +1,17 @@
 import NavbarDeskTop from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
 import PopularBikes from "../../components/popular-bikes/popular-bikes";
+import OurServices from "../../components/our-services/our-services";
 
 const Home = () => {
   return (
     <>
       <NavbarDeskTop />
       <PopularBikes />
+      <OurServices />
       <Footer />
     </>
   );
 };
 
 export default Home;
-
