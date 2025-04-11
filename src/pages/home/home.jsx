331dfdc85462ab5a2f746bef86dfa17826f352b0
Dev-GopiGk <gobikannan710@gmail.com>
2025-04-11 23:11:17 +0530

    11-04-25 - NeedHelp, Ourservices, Popularbikes

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import NavbarDeskTop from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
 import PopularBikes from "../../components/popular-bikes/popular-bikes";
 import OurServices from "../../components/our-services/our-services";
+import NeedHelp from "../../components/need-help/need-help";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <NavbarDeskTop />
       <PopularBikes />
       <OurServices />
+      <NeedHelp />
       <Footer />
     </>
   );
