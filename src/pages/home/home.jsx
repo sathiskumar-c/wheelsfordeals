2f3365b3a961707985d9414c5545a85cfdb09f2c
sathiskumar-c <sathiskumarc0304@gmail.com>
2025-04-14 12:46:04 +0530

    14.04.2025 - frequently-asked-questions

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
+import "./home.scss";
 import NavbarDeskTop from "../../components/navbar/navbar";
 import Footer from "../../components/footer/footer";
 import PopularBikes from "../../components/popular-bikes/popular-bikes";
-import UsedBikeFaq from "../../components/faq-questions/faq-questions";
+import FrequentlyAskedQuestions from "../../components/frequently-asked-questions/frequently-asked-questions";
 import OurServices from "../../components/our-services/our-services";
 import NeedHelp from "../../components/need-help/need-help";
 import Insights from "../../components/insight-driven/insight-driven";
 import TopRecommendedBrand from "../../components/top-recommended-brand/top-recommended-brand";
-import "./home.scss";
 import BrowseBikesBy from "../../components/browse-bikes/browse-bikes";
 import GetUpdates from "../../components/get-updates/get-updates";
 
@@ -20,7 +20,7 @@ const Home = () => {
       <TopRecommendedBrand />
       <NeedHelp />
       <Insights />
-      <UsedBikeFaq />
+      <FrequentlyAskedQuestions />
       <GetUpdates />
       <Footer />
     </>
