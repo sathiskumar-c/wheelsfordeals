63f57118fc0eeb6945205b3a73571645aee84759
Dev-GopiGk <gobikannan710@gmail.com>
2025-05-04 12:25:09 +0530

    04-05-25 - Faq Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home/home";
 import About from "./pages/about/about";
 import ContactUs from "./pages/contactus/contactus";
+import FrequentlyAskedQuestions from "./pages/faq/faq";
 import "./App.scss";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/faq" element={<FrequentlyAskedQuestions />} />
       </Routes>
     </Router>
   );
