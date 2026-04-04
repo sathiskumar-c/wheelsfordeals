7650e484b3bfbca6926418c3fd652e99ff9f7f36
sathiskumar-c <sathiskumarc0304@gmail.com>
2026-04-05 01:11:54 +0530

    FAQ - re designed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -13,7 +13,6 @@ import WhyChooseUs from "../../components/why-choose-us/why-choose-us";
 import ServiceCards from "../../components/services-slider/services-slider";
 import MotivateUs from "../../components/motivate-us/motivate-us";
 import HorizontalTabs from "../../components/common-components/horizontal-tabs/horizontal-tabs";
-import Content from "../../components/common-components/content/content";
 import ImageCardSlider from "../../components/common-components/image-card-slider/image-card-slider";
 
 // JSON Imports
@@ -46,16 +45,7 @@ const Home = () => {
       <ServiceCards />
       <WhyChooseUs />
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <FrequentlyAskedQuestions JSON={FAQ} />
-          </div>
-          <div className="col-md-6">
-            <Content JSON={FAQ.faq_content} />
-          </div>
-        </div>
-      </div>
+      <FrequentlyAskedQuestions JSON={FAQ} />
 
       <GetUpdates />
       <MotivateUs />
